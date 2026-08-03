dd79e92903892f5904dd86e9478e6c392293cef8
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-03 18:33:37 +0530

    add array.js

---
+++ Questions/array.js
@@ -0,0 +1,68 @@
+//31
+ const movies=['abcd','kanchana','alpha','jawan']
+
+//  console.log(movies);
+//32
+//console.log(movies[1])
+
+
+//33
+
+// movies.unshift('saiyara');
+// movies.unshift('Rustam');
+
+// console.log(movies);
+
+//34
+
+// movies.pop()
+// console.log(movies);
+
+//35
+//console.log(movies.slice(0,3));
+ 
+//36
+
+//console.log(movies.indexOf('alpha'));
+
+//37
+
+ //console.log(movies.includes('alpha'));
+  
+
+
+//  if(movies.includes=='alpha')
+//  {
+//     console.log(true);
+    
+//  }
+//38    
+ let arr1=[1,2,3];
+ let arr2=[3,4,5];
+ //console.log(arr1.concat(arr2));
+ 
+//39
+
+ let  a=[5,2,9,1]
+
+ //console.log(a.sort());
+ 
+//40
+
+let b=[...movies];
+//console.log(b);
+
+let c=[]
+movies.forEach(function(value)
+{
+    c.push(value)
+})
+         
+console.log(c);
+
+//console.log(movies.copyWithin(0,2));
+ 
+
+
+
+ 
--- Questions/loops.js
+++ Questions/loops.js
@@ -42,17 +42,90 @@
 // }
 //25
 
-let i=5;
-do {
+// let i=5;
+// do {
   
-    console.log(i);
-    i--;
+//     console.log(i);
+//     i--;
     
     
-} while (i>=1);
+// } while (i>=1);
 //26
+//     function fact()
+//     {
+//          let fact=1;
+
+//          for(let i=5;i>0;i--)
+//          {
+//             fact=fact*i;
+//          }
+//          return fact
+
+//     }
+//    // fact()
+//     console.log(fact());
+    
+    
 //27
+
+// outer loop is  for row and inner i for column 
+// i aur j sirf loops ko control karte hain.
+
+// i batata hai kitni rows banengi.
+// j batata hai ek row me kitne numbers honge.
+// num batata hai kaunsa number print karna hai.
+
+// Isliye i aur j ka kaam sirf loop chalana hai. row me jo add ho raha hai, wo num hai
+    // let num=1;
+    //  for(let i=1;i<=3;i++)
+    //  { 
+    //       let row='';
+    //     for(let j=1; j<=3;j++)
+    //     {
+    //         row+=num+" ";
+    //         num++;
+            
+            
+    //     }
+    //      console.log(` ${row} `);
+    //  }
+
 //28
+
+// let arr=[1,2,3,4];
+// for (let i = arr.length-1; i>=0;i--) {
+//    // arr[i];
+//     console.log(arr[i]);
+    
+// }
+
 //29
+
+// let num=1;
+// while (num<=100) {
+//     if(num%5==0)
+//     {
+//         console.log(num);
+        
+//     }
+//      num++;  
+// }
 //30
+
+// let obj={
+//     name:"priyanka",
+//     class:"msc it",
+//     fees:1000
+// }
+
+// for (const key in obj) {
+ 
+    
+//     const element = obj[key];
+
+//     console.log(element);
+    
+    
+    
+// }
 
