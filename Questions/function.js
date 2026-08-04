4c63085217b696c447b7199d36e655f3cb0f0ac0
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-04 21:42:56 +0530

    add  functions

---
+++ Questions/function.js
@@ -0,0 +1,150 @@
+// 41
+// function EvenOdd()
+// {
+//     let num=-1;
+//     if(num%2==0)
+//     {
+//         console.log("the number is even");
+        
+//     }
+//     else if(num<0)
+//     {
+//         console.log("number is negative");
+        
+//     }
+
+//     else{
+//         console.log("number is odd");
+        
+//     }
+// }
+
+//  EvenOdd();
+// 42
+
+//  function Area_of_circle()
+//  {
+//     let radius=20;
+   
+//     let result= Math.PI*radius*radius;
+
+//     console.log(result);  
+//  }
+
+//  Area_of_circle()
+
+// 43
+
+    // function Sumarr(arr)
+    // {
+    //     let sum=0;
+    //    arr.forEach( v => {
+        
+    //     sum=sum+v;
+    //    });
+
+    //    return sum;
+    // }
+
+    // console.log( Sumarr([10,20,30,40,50]));
+    
+// 44
+
+//  function strCheck(str,char)
+//  {
+//      return str.startsWith(char)
+     
+//  }
+//   console.log(strCheck("priyanka","p"));
+  
+// 45
+//  function max_two_no(num1,num2)
+//  {
+//     if(num1>num2)
+//     {
+//          return "num1 is greater";
+        
+//     }
+//     else if(num2>num1)
+//     {
+//        return "num2 is greater";
+        
+//     }
+//     else{
+//        return "zero";
+        
+//     }
+
+//  }
+
+//  console.log(max_two_no(2,3));
+ 
+// 46
+
+//  function fact_cal(n)
+//  {
+//     let fact=1;
+
+//     for(let i=1;i<=n;i++)
+//     {
+//         fact=fact*i
+//     }
+
+//     return fact
+   
+//  }
+
+//  console.log(fact_cal(5));
+ 
+// 47
+
+//  function reverse_str(str)
+//  {
+//      return str.split('').reverse().join('')
+
+//  }
+
+//  console.log(reverse_str("priya"));
+ 
+// 48
+//     function largest_num_arr(arr)
+//     {
+//         let max=0;
+
+//         for(let i=1;i<arr.length;i++)
+//         {
+//             if(arr[i] > arr[max])
+//             {
+//                 max=i;
+//             }
+//         }
+//         return arr[max]
+
+//     }
+//    console.log(largest_num_arr([30,40,50,60,70]));
+   
+// 49
+
+// function kebabCase(str)
+// {
+//     return str.split(' ').join('-');
+// }
+
+// console.log( kebabCase("priyanka rawat"));
+
+
+// 50
+
+ function greet()
+ {
+    console.log("hello world");
+    
+
+ }
+
+ greet();
+  greet();
+   greet();
+ 
+
+ 
